c7de766c63567d61816361f58e68852e9d3e4eb7
Benjamin <wangbin042@gmail.com>
2019-05-08 11:20:24 +0000

    登录成功后导航逻辑

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -32,8 +32,8 @@ class UsersController extends Controller
         ]);
 
 
-        session()->flash('success','欢迎光临');
-
-        return redirect()->route('users.show',[$user]);
+        Auth::login($user);
+        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+        return redirect()->route('users.show', [$user]);
     }
 }
